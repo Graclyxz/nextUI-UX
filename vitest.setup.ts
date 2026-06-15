013dbbc449b6fbf29075aaa6f65b217d75aeb3fa
JoseVill <jxavier2525@gmail.com>
2026-06-15 15:08:23 -0500

    chore: vitest with jsdom, testing-library, and axe matchers

---
+++ vitest.setup.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom/vitest'
+import 'vitest-axe/extend-expect'
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+
+afterEach(() => cleanup())
