920d26d7ab8f567333643cd779a5cfe42d146efc
JoseVill <jxavier2525@gmail.com>
2026-06-15 15:35:22 -0500

    feat: Tabs on radix primitive with list, trigger, content

---
+++ src/components/Tabs/Tabs.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from './Tabs'
+
+const meta: Meta<typeof Tabs> = {
+  title: 'Components/Tabs',
+  component: Tabs,
+}
+export default meta
+type Story = StoryObj<typeof Tabs>
+
+export const Default: Story = {
+  render: () => (
+    <Tabs defaultValue="account" className="w-80">
+      <TabsList>
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">Account settings panel.</TabsContent>
+      <TabsContent value="password">Password settings panel.</TabsContent>
+    </Tabs>
+  ),
+}
